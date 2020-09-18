f09baea50b0be97d21da63fb1e86184bbb46ec0d
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-09-18 12:37:56 -0400

    added sensorService.js file

--- src/bluetooth/centralModule.ts
+++ src/bluetooth/centralModule.ts
@@ -1,11 +1,40 @@
 const noble = require('@abandonware/noble');
 
 
-
+var serviceUUIDs = []; // default: [] => all
+var allowDuplicates = false;
 
 
 noble.on('stateChange', async (state) => {
-  if (state === 'poweredOn') {
-    await noble.startScanningAsync(['180f'], false);
+  if (state === 'poweredOn')
+  //should I use startScanningAsync or startScanning?
+    noble.startScanning(serviceUUIDs, allowDuplicates[, (error) => if(error) console.log(error)]);
   }
 });
+
+noble.on('discover', (peripheral) => {
+  console.log(`peripheral discovered (${peripheral.id} with address <${peripheral.address}, ${peripheral.addressType}>, connectable ${peripheral.connectable}, RSSI ${peripheral.rssi}:`);
+  console.log(`${peripheral.advertisement.localName}`);
+  console.log(`${JSON.stringify(peripheral.advertisement.serviceUuids)}`);
+})
+
+
+// noble.on('scanStart', callback);
+// noble.stopScanning();
+// noble.on('scanStop', callback);
+// noble.on('discover', callback(peripheral));
+// noble.on('warning', callback(message));
+// peripheral.connect([callback(error)]);
+// peripheral.once('connect', callback);
+// peripheral.disconnect([callback(error)]);
+// peripheral.once('disconnect', callback);
+// peripheral.updateRssi([callback(error, rssi)]);
+// peripheral.once('rssiUpdate', callback(rssi));
+// peripheral.discoverServices(); // any service UUID
+//
+// var serviceUUIDs = ['<service UUID 1>', ...];
+// peripheral.discoverServices(serviceUUIDs[, callback(error, services)]); // particular UUIDs
+// peripheral.discoverAllServicesAndCharacteristics([callback(error, services, characteristics)]);
+// var serviceUUIDs = ['<service UUID 1>', ...];
+// var characteristicUUIDs = ['<characteristic UUID 1>', ...];
+// peripheral.discoverSomeServicesAndCharacteristics(serviceUUIDs, characteristicUUIDs, [callback(error, services, characteristics));
--- src/bluetooth/peripheral.ts
+++ src/bluetooth/peripheral.ts
@@ -1,5 +1,7 @@
 const bleno = require('bleno');
 
+
+//constructor, then create 'new'
 var PrimaryService = bleno.PrimaryService;
 
 // state = <"unknown" | "resetting" | "unsupported" | "unauthorized" | "poweredOff" | "poweredOn">
@@ -28,23 +30,48 @@ var PrimaryService = bleno.PrimaryService;
 var name = "rb-0"; //uuid- rb-0 through rb-5 for each pi
 //UUID generated from guidgenerator.com
 // unsure if we are using a pre-defined UUID or generating one ourselves
-// eventually will be in process.env
-var serviceUuids = ["ca039092-a517-4c2b-90b5-0976fc2e8726"];
+// eventually will be in process.env?
+var serviceUuids = ["fffffffffffffffffffffffffffffff0"];
+//define characteristics and then add to services
+// export const testService
+//export characteristics 
+const testService = new PrimaryService({
+    uuid: 'fffffffffffffffffffffffffffffff0', // should get as env.var
+    characteristics: [
+        // see Characteristic for data type
+    ]
+});
 
 // state must be poweredOn to start advertising
-bleno.on('stateChange', (state) => {
+bleno.on('stateChange', (state: String) => {
   console.log('on stateChange ' + state);
 
   if(state === 'poweredOn') {
-    bleno.startAdvertising(name, serviceUUids[, (error) => if(error) console.log(error)]);
+    bleno.startAdvertising(name, serviceUUids[, (err: any) => if(err) console.log(err)]);
 
+  } else {
+    bleno.stopAdvertising();
   }
 });
 
+bleno.on('advertisingStart', (err: any) => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('advertising...');
+    bleno.setServices([
+      testService
+    ])
+  }
+})
 
+bleno.on('accept', (clientAddress: String) => {
+  console.log(`clientAddress ${clientAddress} accepted`);
 
+  //stop advertising? (is automatic?)
+})
 
-var PrimaryService = new PrimaryService({
-  uuid: serviceUUids[0],
-
+bleno.on('disonnect', (clientAddress: String) => {
+  console.log(`clientAddress ${clientAddress} disconnected`);
+  //try to reconnect
 })
